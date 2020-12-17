a06fcd56f14084d9f7db94e9626a6ababc47f6d5
BPJ94487 <BPJ94487@bethel.edu>
2020-12-17 14:50:10 -0600

    cards are showing and the routes are solid with other things

--- src/components/FavoritesList/FavoritesList.js
+++ src/components/FavoritesList/FavoritesList.js
@@ -1,10 +1,16 @@
 import { connect } from "react-redux";
 import React, { Component } from "react";
+import './FavoritesList.css'
 import FavoritesListItem from "../FavoritesListItem/FavoritesListItem";
 
 class FavoritesList extends Component {
+
+  state = {
+    category: ''
+  }
+
   componentDidMount() {
-    this.props.dispatch({type: 'FETCH_FAVORITES'});
+    this.props.dispatch({type: 'FETCH_FAVORITES' });//tack on an additional parameter from the category to find the get.
     console.log ('in mount');
   }
 
@@ -12,10 +18,33 @@ class FavoritesList extends Component {
     this.props.dispatch({type: 'FETCH_FAVORITES'});
   };
  
+  handleNameChange = (event) => {
+    console.log('event happended')
+    this.setState({
+        category: event.target.value,
+    });
+  }
+
   render() {
     return (
       <div className="favoritesList">
         <h2>Your Selected Favorites</h2>
+
+          <p>&nbsp;</p>
+            <label>Choose a Category:</label>
+            <select id="rating" name="rating" onChange={(event)=>this.handleNameChange(event)}>
+                <option value="All">All</option>
+                <option value="Funny">Funny</option>
+                <option value="Cohort">Cohort</option>
+                <option value="Cartoon">Cartoon</option>
+                <option value="NSFW">NSFW</option>
+                <option value="MEME">MEME</option>
+                
+            </select>
+            <p> </p>
+
+          --
+
         <ul>
           {/* {JSON.stringify(this.props.reduxStore.favoritesList)} */}
           {this.props.reduxStore.favoritesList.map((favorite, index) => {
