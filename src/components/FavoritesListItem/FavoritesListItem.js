939705adb8c6ab6183cf636cbe939b4f331f296c
BPJ94487 <BPJ94487@bethel.edu>
2020-12-17 13:12:40 -0600

    OK how about now

---
+++ src/components/FavoritesListItem/FavoritesListItem.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+
+class FavoritesListItem extends Component {
+
+    state = {
+        showAdd: true,
+        local: ''
+    }
+ 
+    togglAddRemove = () => {
+        this.setState({
+            showAdd: !this.state.showAdd // flip the boolean using ! NOT
+        })
+        this.addPizza()
+    }
+
+    addPizza = () => {
+        if (this.state.showAdd === true) {
+            this.setState({
+                pizzaChoice: this.props.pizza.id
+            })
+            console.log('PizzaListItem addPizza state is true', this.props.pizza.id)
+        }
+        else if (this.state.showAdd === false) {
+            console.log('PizzaListItem addPizza state is false')
+        }
+    }
+
+    sendToStore = () => {
+        
+    }
+ 
+    render() {
+
+        return (
+            <div className="card">
+                <img height="150px" src={this.props.favorite.url} alt={this.props.favorite.title}/>
+                <h2>{this.props.favorite.title}</h2>
+                <p>description: {this.props.favorite.title}</p>
+                <p>price: {this.props.favorite.category}</p>
+                <section>
+                    { this.state.showAdd && // if this part is false, the next part won't show
+                        `Name: ${this.props.favorite.name}` } 
+                </section> 
+
+                <button onClick={this.togglAddRemove}>
+                    {this.state.showAdd ? `Remove From Favorite!` : `Remove From NOT Favorite!`}
+                </button>
+            </div>
+        )
+    }
+}
+const putReduxStateOnProps = (reduxState) => ({ reduxState })
+export default connect(putReduxStateOnProps)(FavoritesListItem);
